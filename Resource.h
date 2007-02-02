fc8c454b7ce41f092dbb4aace9b58850c73692f8
Georgi D. Sotirov <gdsotirov@dir.bg>
2007-02-02 22:02:38 +0000

    Add signal parameters dialog resources

--- Resource.h
+++ Resource.h
@@ -1,19 +1,27 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by SIVI.RC
+// Microsoft Developer Studio generated include file.
+// Used by sivi.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_SIVITYPE				129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_SIVITYPE                    129
+#define IDD_SIGPARAMS                   131
+#define IDC_SIGTYPE                     1000
+#define IDC_STSIGTYPE                   1001
+#define IDC_SIGAMP                      1002
+#define IDC_SIGFREQ                     1003
+#define IDC_STSIGAMP                    1004
+#define IDC_STSIGFREQ                   1005
+#define ID_FILE_ADDSIGNAL               32771
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_3D_CONTROLS			1
-#define _APS_NEXT_RESOURCE_VALUE	130
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
