1b95bef6b895969332128f400c23b232695c4f7c
Georgi D. Sotirov <gdsotirov@dir.bg>
2007-02-07 20:44:22 +0000

    Initial support for zooming of the x axis

--- SignalView.cpp
+++ SignalView.cpp
@@ -28,6 +28,11 @@ BEGIN_MESSAGE_MAP(CSignalView, CView)
 	ON_COMMAND(ID_FILE_ADDSIGNAL, OnFileAddsignal)
 	ON_UPDATE_COMMAND_UI(ID_FILE_ADDSIGNAL, OnUpdateFileAddsignal)
 	ON_WM_MOUSEMOVE()
+	ON_COMMAND(ID_VIEW_ZOOM_ZOOMIN, OnViewZoomIn)
+	ON_COMMAND(ID_VIEW_ZOOM_ZOOMOUT, OnViewZoomOut)
+	ON_COMMAND(ID_VIEW_ZOOM_ORIGINALSIZE, OnViewOriginalSize)
+	ON_UPDATE_COMMAND_UI(ID_VIEW_ZOOM_ZOOMOUT, OnUpdateViewZoomOut)
+	ON_UPDATE_COMMAND_UI(ID_VIEW_ZOOM_ORIGINALSIZE, OnUpdateViewOriginalSize)
 	//}}AFX_MSG_MAP
 	// Standard printing commands
 	ON_COMMAND(ID_FILE_PRINT, CView::OnFilePrint)
@@ -38,7 +43,11 @@ END_MESSAGE_MAP()
 /////////////////////////////////////////////////////////////////////////////
 // CSignalView construction/destruction
 
-CSignalView::CSignalView() : m_lFontHeight(0) {
+CSignalView::CSignalView() {
+	m_iZoomFactor = 1;
+	m_lFontHeight = 0;
+	m_dPixPerUnitX = 1.0;
+	m_dPixPerUnitY = 1.0;
 }
 
 CSignalView::~CSignalView() {
@@ -89,18 +98,25 @@ void CSignalView::OnDraw(CDC* pDC) {
 	// Draw coordinate system
 	GetClientRect(&m_ClRect);
 
-	int center_x = m_ClRect.right / 2;
-	int center_y = m_ClRect.bottom / 2;
+	m_iCenterX = m_ClRect.right / 2;
+	m_iCenterY = m_ClRect.bottom / 2;
 
-	pDC->MoveTo(m_ClRect.left, center_y);
-	pDC->LineTo(m_ClRect.right, center_y);
-	pDC->MoveTo(center_x, m_ClRect.top);
-	pDC->LineTo(center_x, m_ClRect.bottom);
+	pDC->MoveTo(m_ClRect.left, m_iCenterY);
+	pDC->LineTo(m_ClRect.right, m_iCenterY);
+	pDC->MoveTo(m_iCenterX, m_ClRect.top);
+	pDC->LineTo(m_iCenterX, m_ClRect.bottom);
 
 	double amp_sum = pDoc->CalcAmplitude();
 	if ( amp_sum != 0 ) {
-		double unit_in_pixels = (center_y - m_lFontHeight - TEXT_OFFSET * 2) / amp_sum;
-		double start_x_units = - center_x / unit_in_pixels;
+		m_dPixPerUnitY = (m_iCenterY - m_lFontHeight - TEXT_OFFSET * 2) / amp_sum;
+		m_dPixPerUnitX = m_dPixPerUnitY;
+
+		double start_x_units = - m_iCenterX / m_dPixPerUnitX;
+
+		if ( m_iZoomFactor != 1 ) {
+			m_dPixPerUnitX *= m_iZoomFactor;
+			start_x_units /= m_iZoomFactor;
+		}
 
 		CString number;
 
@@ -108,12 +124,12 @@ void CSignalView::OnDraw(CDC* pDC) {
 		double y_unit = -floor(amp_sum);
 		while ( y_unit <= amp_sum ) {
 			if ( y_unit != 0 ) {
-				int y_pix = (y_unit > 0) ? center_y - (int)(fabs(y_unit) * unit_in_pixels) : center_y + (int)(fabs(y_unit) * unit_in_pixels);
+				int y_pix = (y_unit > 0) ? m_iCenterY - (int)(fabs(y_unit) * m_dPixPerUnitY) : m_iCenterY + (int)(fabs(y_unit) * m_dPixPerUnitY);
 
-				pDC->MoveTo(center_x - 2, y_pix);
-				pDC->LineTo(center_x + 2, y_pix);
+				pDC->MoveTo(m_iCenterX - 2, y_pix);
+				pDC->LineTo(m_iCenterX + 2, y_pix);
 				number.Format("%.2f", y_unit);
-				pDC->TextOut(center_x + 5, y_pix - (m_lFontHeight / 2), number);
+				pDC->TextOut(m_iCenterX + 5, y_pix - (m_lFontHeight / 2), number);
 			}
 
 			y_unit += 1.0;
@@ -123,12 +139,12 @@ void CSignalView::OnDraw(CDC* pDC) {
 		double x_unit = ceil(start_x_units);
 		while ( x_unit <= -ceil(start_x_units) ) {
 			if ( x_unit != 0 ) {
-				int x_pix = (x_unit > 0) ? center_x + (int)(fabs(x_unit) * unit_in_pixels) : center_x - (int)(fabs(x_unit) * unit_in_pixels);
+				int x_pix = (x_unit > 0) ? m_iCenterX + (int)(fabs(x_unit) * m_dPixPerUnitX) : m_iCenterX - (int)(fabs(x_unit) * m_dPixPerUnitX);
 
-				pDC->MoveTo(x_pix, center_y - 2);
-				pDC->LineTo(x_pix, center_y + 2);
+				pDC->MoveTo(x_pix, m_iCenterY - 2);
+				pDC->LineTo(x_pix, m_iCenterY + 2);
 				number.Format("%.2f", x_unit);
-				pDC->TextOut(x_pix - (m_lFontHeight / 2), center_y + 5, number);
+				pDC->TextOut(x_pix - (m_lFontHeight / 2), m_iCenterY + 5, number);
 			}
 
 			x_unit += 1.0;
@@ -140,14 +156,14 @@ void CSignalView::OnDraw(CDC* pDC) {
 		double x = start_x_units;
 		while ( x <= -start_x_units ) {
 			double y = pDoc->Calc(x);
-			int x_pix = (int)floor(x * unit_in_pixels + center_x);
-			int y_pix = (int)floor(y * unit_in_pixels + center_y);
+			int x_pix = (int)floor(x * m_dPixPerUnitX + m_iCenterX);
+			int y_pix = (int)floor(y * m_dPixPerUnitY + m_iCenterY);
 
 			if ( x == start_x_units )
 				pDC->MoveTo(x_pix, y_pix);
 			pDC->LineTo(x_pix, y_pix);
 
-			x += 0.001;
+			x += 1 / m_dDiscretFreq;
 		}
 	}
 
@@ -192,23 +208,24 @@ CSignalDoc* CSignalView::GetDocument() const { // non-debug version is inline
 // CSignalView message handlers
 
 void CSignalView::OnLButtonDblClk(UINT nFlags, CPoint point) {
-	CSignalDoc * doc = GetDocument();
+	CSignalDoc * pDoc = GetDocument();
 	CSigPrmsDlg sig_params;
 
-	for ( int i = 0; i < doc->SignalsCount(); ++i ) {
-		sig_params.m_stype = doc->GetType(i);
-		CSignal * sig = doc->GetSignal(i);
-		sig_params.m_samp  = sig->getAmplitude();
-		sig_params.m_sfreq = sig->getFrequency();
+	for ( int i = 0; i < pDoc->SignalsCount(); ++i ) {
+		sig_params.m_stype = pDoc->GetType(i);
+		CSignal * pSig = pDoc->GetSignal(i);
+		sig_params.m_samp  = pSig->getAmplitude();
+		sig_params.m_sfreq = pSig->getFrequency();
 
 		if ( sig_params.DoModal() == IDOK ) {
 			CString new_type = sig_params.m_stype.Mid(0, 3);
 			new_type.MakeLower();
-			doc->ChangeType(i, new_type);
-			sig->setAmplitude(sig_params.m_samp);
-			sig->setFrequency(sig_params.m_sfreq);
-			doc->SetModifiedFlag();
-			doc->UpdateAllViews(NULL);
+			pDoc->ChangeType(i, new_type);
+			pSig->setAmplitude(sig_params.m_samp);
+			pSig->setFrequency(sig_params.m_sfreq);
+			m_dDiscretFreq = 2 * pSig->getFrequency();
+			pDoc->SetModifiedFlag();
+			pDoc->UpdateAllViews(NULL);
 		}
 	}
 
@@ -244,20 +261,49 @@ void CSignalView::OnMouseMove(UINT nFlags, CPoint point) {
 	CStatusBar * pStatus = pFrame->GetStatusBar();
 	ASSERT_VALID(pStatus);
 
-	//GetClientRect(&cl_rect);
-
-	int center_x = m_ClRect.right / 2;
-	int center_y = m_ClRect.bottom / 2;
-
 	double amp_sum = pDoc->CalcAmplitude();
 	if ( amp_sum > 0 ) {
-		double unit_in_pixels = (center_y - m_lFontHeight - TEXT_OFFSET * 2) / amp_sum;
-
-		double x_unit = (point.x - center_x) / unit_in_pixels;
-		double y_unit = - (point.y - center_y) / unit_in_pixels;
+		double x_unit = (point.x - m_iCenterX) / m_dPixPerUnitX;
+		double y_unit = - (point.y - m_iCenterY) / m_dPixPerUnitY;
 		buf.Format(IDS_COORDINATES, x_unit, y_unit);
 		pStatus->SetPaneText(0, buf);
 	}
 
 	CView::OnMouseMove(nFlags, point);
+}
+
+void CSignalView::OnViewZoomIn() {
+	CSignalDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	m_iZoomFactor++;
+
+	pDoc->UpdateAllViews(NULL);
+}
+
+void CSignalView::OnViewZoomOut() {
+	CSignalDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	if ( m_iZoomFactor > 1 )
+		m_iZoomFactor--;
+
+	pDoc->UpdateAllViews(NULL);
+}
+
+void CSignalView::OnViewOriginalSize() {
+	CSignalDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+
+	m_iZoomFactor = 1;
+
+	pDoc->UpdateAllViews(NULL);
+}
+
+void CSignalView::OnUpdateViewZoomOut(CCmdUI* pCmdUI) {
+	pCmdUI->Enable(m_iZoomFactor != 1);
+}
+
+void CSignalView::OnUpdateViewOriginalSize(CCmdUI* pCmdUI) {
+	pCmdUI->Enable(m_iZoomFactor != 1);	
 }
