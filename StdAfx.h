9d4cba31bb367ea003100ef84880c3cb09dfddae
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-11-29 22:07:07 +0200

    Define WIN32_LEAN_AND_MEAN

--- StdAfx.h
+++ StdAfx.h
@@ -10,13 +10,15 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+/* See https://docs.microsoft.com/en-us/windows/win32/winprog/using-the-windows-headers */
+#define WIN32_LEAN_AND_MEAN // exclude APIs such as Cryptography, DDE, RPC, Shell, and Windows Sockets
+#define VC_EXTRALEAN        // Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#include <afxdtctl.h>       // MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+#include <afxcmn.h>         // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
